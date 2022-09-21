d8155bc913036a673daa78f488372af418286081
paviethra <62457057+paviethra@users.noreply.github.com>
2022-09-21 12:03:12 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "b605c7d449mshb51272453d2f8f2p187e1fjsn6c6fce4d5ad7",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+
